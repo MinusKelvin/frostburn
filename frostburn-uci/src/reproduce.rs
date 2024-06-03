cd8b82cdbe35c6a4dd9ef6eae0febcd08b58d462
MinusKelvin <mark.carlson@minuskelvin.net>
2024-06-03 16:52:22 +1000

    add ob pgn reproduction ability
    bench: 4935081

---
+++ frostburn-uci/src/reproduce.rs
@@ -0,0 +1,84 @@
+use std::io::prelude::Read;
+use std::io::stdin;
+use std::time::Instant;
+
+use cozy_chess::util::parse_san_move;
+use cozy_chess::{Board, Color};
+use frostburn::{Limits, LocalData, Search, SharedData};
+
+use crate::MoveFormat;
+
+pub fn reproduce(side: Color, mb: usize) {
+    let mut board = Board::startpos();
+
+    for line in stdin()
+        .lines()
+        .map(Result::unwrap)
+        .take_while(|l| !l.is_empty())
+    {
+        if line.starts_with("[FEN") {
+            board = line.split('"').nth(1).unwrap().parse().unwrap();
+        }
+    }
+
+    let mut shared = SharedData::new(mb);
+    let mut local = LocalData::new();
+    let mut history = vec![];
+
+    let mut text = String::new();
+    stdin().read_to_string(&mut text).unwrap();
+    let mut tokens = text
+        .split_ascii_whitespace()
+        .inspect(|token| print!("{token} "));
+
+    let mut last_mvnum = "";
+
+    loop {
+        if board.side_to_move() == Color::White {
+            last_mvnum = match tokens.next() {
+                Some(v) => v,
+                None => break,
+            };
+        } else {
+            print!("{last_mvnum}.. ");
+        }
+
+        let mv = parse_san_move(&board, tokens.next().unwrap()).unwrap();
+        let nodes = tokens
+            .by_ref()
+            .nth(3)
+            .unwrap()
+            .trim_matches(|c: char| !c.is_ascii_digit())
+            .parse()
+            .unwrap();
+
+        println!();
+
+        if board.side_to_move() == side {
+            let start = Instant::now();
+            shared.prepare_for_search();
+            Search {
+                root: &board,
+                history: history.clone(),
+                clock: &|| start.elapsed(),
+                info: &mut |info| {
+                    super::print_info(&board, MoveFormat::Chess960, &info);
+                    if info.finished {
+                        assert_eq!(info.nodes, nodes);
+                        assert_eq!(info.pv[0], mv);
+                    }
+                },
+                data: &mut local,
+                shared: &shared,
+                limits: Limits {
+                    nodes: Some(nodes),
+                    ..Default::default()
+                },
+            }
+            .search();
+        }
+
+        history.push(board.hash());
+        board.play(mv);
+    }
+}
