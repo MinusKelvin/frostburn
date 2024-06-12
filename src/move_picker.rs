066b6afbd1c4e63b092bd38c1e962306278c6a61
MinusKelvin <mark.carlson@minuskelvin.net>
2024-06-12 11:21:38 +1000

    use SEE for capture ordering
    bench: 6488018

--- src/move_picker.rs
+++ src/move_picker.rs
@@ -1,5 +1,8 @@
 use arrayvec::ArrayVec;
-use cozy_chess::{Board, Move, Piece};
+use cozy_chess::{
+    get_bishop_moves, get_king_moves, get_knight_moves, get_pawn_attacks, get_rook_moves, BitBoard,
+    Board, Color, Move, Piece, Square,
+};
 
 use crate::history::PieceHistory;
 use crate::LocalData;
@@ -39,7 +42,10 @@ impl<'a> MovePicker<'a> {
             for mv in mvs {
                 let mut score = match tt_mv {
                     Some(tt_mv) if mv == tt_mv => 1_000_000,
-                    _ if opp.has(mv.to) => 100_000 + board.piece_on(mv.to).unwrap() as i32,
+                    _ if opp.has(mv.to) => {
+                        let see_score = see(board, mv);
+                        110_000 + see_score + board.piece_on(mv.to).unwrap() as i32
+                    }
                     _ => {
                         data.history.get(board, mv) as i32
                             + counter_hist.map_or(0, |table| table.get(board, mv) as i32)
@@ -92,4 +98,95 @@ impl<'a> MovePicker<'a> {
     pub fn has_moves(&self) -> bool {
         self.has_moves
     }
+}
+
+fn see(pos: &Board, mv: Move) -> i32 {
+    const VALUES: [i32; 6] = [100, 300, 325, 500, 900, 0];
+
+    fn see_impl(pos: &Board, occupied: BitBoard, sq: Square, stm: Color, piece: Piece) -> i32 {
+        let pawns = occupied & get_pawn_attacks(sq, !stm) & pos.colored_pieces(stm, Piece::Pawn);
+        if let Some(pawn) = pawns.next_square() {
+            if piece == Piece::King {
+                return -99999;
+            }
+            return 0.max(
+                VALUES[piece as usize]
+                    - see_impl(pos, occupied - pawn.bitboard(), sq, !stm, Piece::Pawn),
+            );
+        }
+
+        let knights = occupied & get_knight_moves(sq) & pos.colored_pieces(stm, Piece::Knight);
+        if let Some(knight) = knights.next_square() {
+            if piece == Piece::King {
+                return -99999;
+            }
+            return 0.max(
+                VALUES[piece as usize]
+                    - see_impl(pos, occupied - knight.bitboard(), sq, !stm, Piece::Knight),
+            );
+        }
+
+        let bishops =
+            occupied & get_bishop_moves(sq, occupied) & pos.colored_pieces(stm, Piece::Bishop);
+        if let Some(bishop) = bishops.next_square() {
+            if piece == Piece::King {
+                return -99999;
+            }
+            return 0.max(
+                VALUES[piece as usize]
+                    - see_impl(pos, occupied - bishop.bitboard(), sq, !stm, Piece::Bishop),
+            );
+        }
+
+        let rooks = occupied & get_rook_moves(sq, occupied) & pos.colored_pieces(stm, Piece::Rook);
+        if let Some(rook) = rooks.next_square() {
+            if piece == Piece::King {
+                return -99999;
+            }
+            return 0.max(
+                VALUES[piece as usize]
+                    - see_impl(pos, occupied - rook.bitboard(), sq, !stm, Piece::Rook),
+            );
+        }
+
+        let queens = occupied
+            & (get_rook_moves(sq, occupied) | get_bishop_moves(sq, occupied))
+            & pos.colored_pieces(stm, Piece::Queen);
+        if let Some(queen) = queens.next_square() {
+            if piece == Piece::King {
+                return -99999;
+            }
+            return 0.max(
+                VALUES[piece as usize]
+                    - see_impl(pos, occupied - queen.bitboard(), sq, !stm, Piece::Queen),
+            );
+        }
+
+        let kings = occupied & get_king_moves(sq) & pos.colored_pieces(stm, Piece::King);
+        if let Some(king) = kings.next_square() {
+            if piece == Piece::King {
+                return -99999;
+            }
+            return 0.max(
+                VALUES[piece as usize]
+                    - see_impl(pos, occupied - king.bitboard(), sq, !stm, Piece::King),
+            );
+        }
+
+        0
+    }
+
+    let captured = pos
+        .piece_on(mv.to)
+        .map_or(0, |piece| VALUES[piece as usize]);
+    let occupied = pos.occupied() - mv.from.bitboard();
+
+    captured
+        - see_impl(
+            pos,
+            occupied,
+            mv.to,
+            !pos.side_to_move(),
+            pos.piece_on(mv.from).unwrap(),
+        )
 }
