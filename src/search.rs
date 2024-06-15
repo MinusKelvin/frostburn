fecac3e46a386d8736b887c0026593792d829273
MinusKelvin <mark.carlson@minuskelvin.net>
2024-06-16 08:16:21 +1000

    attempt at capture history
    bench: 4692990

--- src/search.rs
+++ src/search.rs
@@ -21,6 +21,7 @@ impl Search<'_> {
             .collect();
 
         self.data.history.decay();
+        self.data.capture_hist.decay();
         self.data.counter_hist.decay();
         self.data.followup_hist.decay();
 
