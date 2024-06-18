3a45aa3965246c84dca22dbd5ba9c8a018dfc42c
MinusKelvin <mark.carlson@minuskelvin.net>
2024-06-18 13:11:13 +1000

    fix negative mate score printing
    bench: 5101922

--- src/eval.rs
+++ src/eval.rs
@@ -50,7 +50,7 @@ impl Eval {
 impl Display for Eval {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         if self.0 < -MAX_NONMATE {
-            write!(f, "mate {}", (self.0 + 30_000) / 2 - 1)
+            write!(f, "mate {}", -(self.0 + 30_000) / 2 + 1)
         } else if self.0 > MAX_NONMATE {
             write!(f, "mate {}", (30_000 - self.0) / 2 + 1)
         } else {
