2a3e029b8f3593a36c1b30d17852b653bf131b02
MinusKelvin <mark.carlson@minuskelvin.net>
2024-06-23 17:56:59 +1000

    fuse crelu activation to output dot product
    bench: 3570468

--- src/nnue/avx2.rs
+++ src/nnue/avx2.rs
@@ -12,23 +12,31 @@ impl Accumulator {
         update(&mut self.white, &updates.white_adds, &updates.white_rms);
         update(&mut self.black, &updates.black_adds, &updates.black_rms);
 
-        let mut activated = [0; 1024];
-        let (left, right) = activated.split_at_mut(512);
-        let left = <&mut [_; 512]>::try_from(left).unwrap();
-        let right = <&mut [_; 512]>::try_from(right).unwrap();
+        let (first, last) = NETWORK.l1.w[0].split_at(512);
+        let first = <&[_; 512]>::try_from(first).unwrap();
+        let last = <&[_; 512]>::try_from(last).unwrap();
 
-        match stm {
-            Color::White => {
-                crelu(left, &self.white);
-                crelu(right, &self.black);
-            }
-            Color::Black => {
-                crelu(left, &self.black);
-                crelu(right, &self.white);
-            }
-        }
+        let (stm, nstm) = match stm {
+            Color::White => (&self.white, &self.black),
+            Color::Black => (&self.black, &self.white),
+        };
 
-        NETWORK.l1.bias[0] as i32 + dot(&activated, &NETWORK.l1.w[0])
+        let result = _mm256_add_epi32(
+            fused_activate_dot(stm, first),
+            fused_activate_dot(nstm, last),
+        );
+
+        let mut result = _mm_add_epi32(
+            _mm256_extracti128_si256::<0>(result),
+            _mm256_extracti128_si256::<1>(result),
+        );
+        // result = A B C D
+        result = _mm_add_epi32(result, _mm_shuffle_epi32::<0b01_00_11_10>(result));
+        // result = A+C B+D A+C B+D
+        result = _mm_add_epi32(result, _mm_shuffle_epi32::<0b10_11_00_01>(result));
+        // result = A+B+C+D A+B+C+D A+B+C+D A+B+C+D
+
+        NETWORK.l1.bias[0] as i32 + _mm_extract_epi32::<0>(result)
     }
 }
 
@@ -71,40 +79,23 @@ unsafe fn partial_update(
 }
 
 #[target_feature(enable = "avx2")]
-unsafe fn crelu(result: &mut [i16; 512], a: &[i16; 512]) {
+#[inline]
+unsafe fn fused_activate_dot(a: &[i16; 512], w: &[i16; 512]) -> __m256i {
     let a: *const __m256i = a.as_ptr().cast();
-    let result: *mut __m256i = result.as_mut_ptr().cast();
+    let w: *const __m256i = w.as_ptr().cast();
+    let mut result = _mm256_setzero_si256();
 
     let zero = _mm256_setzero_si256();
     let one = _mm256_set1_epi16(255);
 
     for i in 0..CHUNKS {
-        let v = _mm256_loadu_si256(a.add(i));
-        let v = _mm256_max_epi16(v, zero);
-        let v = _mm256_min_epi16(v, one);
-        _mm256_storeu_si256(result.add(i), v);
-    }
-}
-
-#[target_feature(enable = "avx2")]
-unsafe fn dot(a: &[i16; 1024], b: &[i16; 1024]) -> i32 {
-    let a: *const __m256i = a.as_ptr().cast();
-    let b: *const __m256i = b.as_ptr().cast();
-    let mut result = _mm256_setzero_si256();
-
-    for i in 0..CHUNKS*2 {
         let a = _mm256_loadu_si256(a.add(i));
-        let b = _mm256_loadu_si256(b.add(i));
-        let v = _mm256_madd_epi16(a, b);
+        let a = _mm256_max_epi16(a, zero);
+        let a = _mm256_min_epi16(a, one);
+        let w = _mm256_loadu_si256(w.add(i));
+        let v = _mm256_madd_epi16(a, w);
         result = _mm256_add_epi32(result, v);
     }
 
-    let mut result = _mm_add_epi32(_mm256_extracti128_si256::<0>(result), _mm256_extracti128_si256::<1>(result));
-    // result = A B C D
-    result = _mm_add_epi32(result, _mm_shuffle_epi32::<0b01_00_11_10>(result));
-    // result = A+C B+D A+C B+D
-    result = _mm_add_epi32(result, _mm_shuffle_epi32::<0b10_11_00_01>(result));
-    // result = A+B+C+D A+B+C+D A+B+C+D A+B+C+D
-
-    _mm_extract_epi32::<0>(result)
+    result
 }
