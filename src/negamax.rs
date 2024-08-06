89bb4c40dc8da80ae9b66bed0c3a9ed728f569f6
MinusKelvin <mark.carlson@minuskelvin.net>
2024-08-07 08:19:50 +1000

    do lmp in PV
    bench: 6879573

--- src/negamax.rs
+++ src/negamax.rs
@@ -109,7 +109,7 @@ impl Search<'_> {
 
             let quiet = !pos.colors(!pos.side_to_move()).has(scored_mv.mv.to);
 
-            if !PV && quiet && !best_score.losing() && i > lmp_limit {
+            if quiet && !best_score.losing() && i > lmp_limit {
                 continue;
             }
 
